6a47257fbe5c7119020d303fc7e3372d252a7b92
Panca <panca21@mhs.unsyiah.ac.id>
2023-04-28 03:20:19 +0700

    ubah

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,18 +18,7 @@ export default function Home() {
             <h1 className="text-4xl font-bold mb-4">
               Jual Beli Sampah Anorganik Online dengan SIBISA (Si Bijak Sampah)
             </h1>
-            <p className="text-lg">
-              Selamat datang di website jual beli sampah anorganik online kami!
-              Kami memudahkan Anda untuk menjual dan membeli berbagai jenis
-              sampah anorganik dengan mudah dan efisien. Dari plastik hingga
-              logam, semua tersedia di sini. Dengan fitur pencarian dan filter,
-              Anda dapat menemukan produk sampah anorganik yang sesuai dengan
-              kebutuhan Anda. Kami juga menyediakan informasi dan tips tentang
-              pengelolaan sampah anorganik yang efektif. Dengan sistem
-              pembayaran yang aman dan mudah digunakan, Anda dapat membeli atau
-              menjual sampah anorganik dengan percaya diri. Jaga lingkungan dan
-              kesehatan manusia dengan bergabung bersama kami sekarang!
-            </p>
+            <p className="text-lg"></p>
           </div>
         </section>
 
